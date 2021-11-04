9987050169dbd7ff08715a9cef534e95eda09495
nurbek717 <nurbektukhtamishev@gmail.com>
2021-11-04 19:43:20 +0500

    salom

---
+++ js/main.js
@@ -0,0 +1,112 @@
+// var userName = "Farrukhbek";
+// var userSurname = "Abbosov";
+
+
+// console.log(userName + " " + userSurname);
+
+// Template literal.
+// console.log(`Assalomu alaykum ${userName} ${userSurname}`);
+
+
+// var x = 8;
+// var y = "25";
+
+// console.log(x + y);
+
+
+// console.log(Number(x) + Number(y));
+
+// console.log((x-0)+(y-0));
+
+// console.log(y, typeof y);
+
+// Subtraction 
+
+// console.log(10-5);
+
+// Multiplication
+
+// console.log(10*5);
+
+// Devision
+
+// console.log(10/5);
+
+// Exponentiation
+
+// console.log(2**20);
+
+// Modulus(Qoldiq)
+
+// console.log(39/2, 39%2);
+
+
+// z++ increment
+// z-- decrement
+
+// var z = "123";
+// z--
+// console.log(typeof z);
+
+
+// Math funcsiyalari
+// var g = 0.5;
+// console.log(Math.ceil(g));
+// console.log(Math.floor(g));
+// console.log(Math.round(g));
+
+// console.log(Math.floor(39/2), 39%2 + " qoldiq");
+
+// console.log(Math.round(Math.random() * 10)); 
+
+
+// var randomRound = Math.random();
+
+// console.log(Math.round(randomRound * 500), randomRound);
+
+// var roundNumber = 30;
+
+// var playerName = "Akmal";
+
+
+
+//                       true                     false
+// if ((roundNumber >= "15") == (roundNumber <= 40) && playerName) {
+//     console.log("TRUE");
+// }else if(true) {
+//     console.log("FALSE");
+// }else {
+
+// }
+
+
+
+
+
+
+
+var nammer = 1200;
+var phone = 1111;
+var isName = 1202;
+
+var jami = nammer+phone+isName;
+console.log(jami);
+
+
+
+var isCty = prompt("qayoqqa ketayapsiz dussim");
+var userName = prompt ("Isminizni kirtishni unitmang agar sizni Suriyaga tashalab ketamiz yulimiz ustida");
+var mony = prompt("Hush kelibsiz tabriklayman sizni bizni hizmatdan foydalanhaning uchin chunki bizning hizmat juda arzon 100000000$ kerak buladi sizga bormi uzi pul")
+
+
+
+
+    if(mony < jami){
+        alert("ketalmay qoldizku " + userName + " dasturchi buganigizda aniq ketardingiz") ;
+        console.log("ketalmay qoldizku " + userName + " dasturchi buganigizda aniq ketardingiz");
+    }
+    else{
+        alert("joniz bor  " + userName +  "  dasturchimidiz") ;
+        console.log("joniz bor " + userName + " dasturchi misiz.");
+    }
+    
